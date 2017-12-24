820056f7bf69d9a0313f88c5005dddc3cd45e9d2
Alex Shand <alexshand4827@gmail.com>
2017-12-24 12:07:05 +0000

    Formatting in MainActivity and a bunch of files turned up

--- app/src/main/java/quick/SMS/MainActivity.kt
+++ app/src/main/java/quick/SMS/MainActivity.kt
@@ -10,100 +10,30 @@ import android.support.v4.app.ActivityCompat
 import android.content.Intent
 import android.net.Uri
 /*
-* https://antonioleiva.com/databases-anko-kotlin/
-* The above link is to do database's, which I think we will need to save and retain each contacts
-* common text messages - which will have to be defined by the user
-*
-* http://sonevalley.blogspot.co.uk/2015/12/retrieving-list-of-contacts-in-android.html
-* A link explaining how to pull up the contacts from the user, in java but Kotlin is interoperable
-*
-*
-* https://www.androidhive.info/2017/07/android-implementing-preferences-settings-screen/
-* Toggle settings for the user
-*
-*I have already inserted compile 'org.jetbrains.anko:anko-sqlite:0.10.0' and synced
-*Alex, I think you'll have to upgrade to at least android studio 3.0 to support Kotlin,
-*I apologise I remember you telling me this is very difficult to do on your computer
-*
-* http://www.vogella.com/tutorials/AndroidDragAndDrop/article.html
-* Drap and drop link above
-*
-* */
+ * https://antonioleiva.com/databases-anko-kotlin/
+ * The above link is to do database's, which I think we will need to save and retain each contacts
+ * common text messages - which will have to be defined by the user
+ *
+ * http://sonevalley.blogspot.co.uk/2015/12/retrieving-list-of-contacts-in-android.html
+ * A link explaining how to pull up the contacts from the user, in java but Kotlin is interoperable
+ *
+ * https://www.androidhive.info/2017/07/android-implementing-preferences-settings-screen/
+ * Toggle settings for the user
+ *
+ * http://www.vogella.com/tutorials/AndroidDragAndDrop/article.html
+ * Drap and drop link above
+ *
+ */
 class MainActivity : AppCompatActivity() {
-    /*The reason I use a var is so it can be changed later,
-    it is good practise to make everything val in Kotlin
-    * unless it needs to be var*/
-    private var TAG = "SMS" /*The idea to this one was to keep track easily of which tab is selected*/
 
-    fun callNumber(phoneNumber: String){
-        val callIntent = Intent(Intent.ACTION_CALL)
-        callIntent.data = Uri.parse(Manifest.permission.CALL_PHONE)
-        try{startActivity(callIntent)}
-        catch(e:SecurityException){
-            requestPermission(Manifest.permission.CALL_PHONE)
-
-        }
-    }
-    fun loadString(key: String): String {
-        /*Function to load a shared preference String*/
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(applicationContext)
-        val savedValue = sharedPreferences.getString(key, "UNKNOWN") /*DEFAULT AS UNKNOWN*/
-        return savedValue
-    }
-    fun saveString(key: String, value: String) {
-        /*Function to save a shared preference String*/
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(applicationContext)
-        val editor = sharedPreferences.edit()
-        editor.putString(key, value)
-        editor.apply()
-    }
-    private fun requestPermission(permission: String) {
-        /*Function to request permission, takes a parameter of the permission we need need
-        * which in this case will be SMS and CALLS permissions. This will only ask for it iff
-        * we do not have the permission, otherwise nothing will happen*/
-        try {
-            val permissions = arrayOf(permission)
-            ActivityCompat.requestPermissions(this, permissions, 1)
-        } catch(e: SecurityException) {
-            /*Catch any Security Exceptions*/
-        }
-    }
-
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.SMS -> {
-                TAG = "SMS" /*Setting tag parameter appropriately*/
-                /* QUICK SMS*/
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.CALL -> {
-                TAG = "CALL" /*Setting tag parameter appropriately*/
-                /* QUICK CALL */
-                return@OnNavigationItemSelectedListener true
-            }
-
-        }
-        false
-    }
-
-    fun onClick(tileNumber: Int){
-        /*The idea for this function is to do the bulk of the work when the user clicks the tile
-        * just using a function to reduce the amount of code */
-
-        if(TAG=="SMS"){
-
-        } else {
-
-
-        }
-
-    }
+    /* The idea to this one was to keep track easily of which tab is selected */
+    private var TAG = "SMS"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        requestPermission(Manifest.permission.SEND_SMS) /*Request permission to send SMS*/
-        requestPermission(Manifest.permission.CALL_PHONE) /*Request permission to make calls*/
+        // Request required permissions
+        requestPermissions(arrayOf(Manifest.permission.SEND_SMS, Manifest.permission.CALL_PHONE))
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
         tile_1.setOnClickListener {
@@ -137,5 +67,61 @@ class MainActivity : AppCompatActivity() {
             onClick(10)
         }
 
+    }
+
+    fun callNumber(phoneNumber: String) {
+        // Calls a phone number
+        val callIntent = Intent(Intent.ACTION_CALL)
+        callIntent.data = Uri.parse(Manifest.permission.CALL_PHONE)
+        try{startActivity(callIntent)}
+        catch(e:SecurityException){
+            requestPermissions(arrayOf(Manifest.permission.CALL_PHONE))
+
+        }
+    }
+    fun loadString(key: String): String {
+        /* Loads a String from Shared Preferences */
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(applicationContext)
+        val savedValue = sharedPreferences.getString(key, "UNKNOWN") /*DEFAULT AS UNKNOWN*/
+        return savedValue
+    }
+    fun saveString(key: String, value: String) {
+        /* Saves a String to Shared Preferences */
+        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(applicationContext)
+        val editor = sharedPreferences.edit()
+        editor.putString(key, value)
+        editor.apply()
+    }
+    private fun requestPermissions(permissions: Array<String>) {
+        /* Request Permission if required */
+        ActivityCompat.requestPermissions(this, permissions, 1)
+    }
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.SMS -> {
+                TAG = "SMS" /* Setting tag parameter appropriately */
+                /* QUICK SMS */
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.CALL -> {
+                TAG = "CALL" /* Setting tag parameter appropriately */
+                /* QUICK CALL */
+                return@OnNavigationItemSelectedListener true
+            }
+
+        }
+        false
+    }
+
+    fun onClick(tileNumber: Int){
+        /* The idea for this function is to do the bulk of the work when the user clicks the tile
+         * just using a function to reduce the amount of code */
+
+        if(TAG=="SMS"){
+
+        } else {
+
+        }
     }
 }
